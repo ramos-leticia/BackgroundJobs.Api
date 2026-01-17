7f6ffbeb79c3342dbeb92e9b41e8e2669daacecb
ramos-leticia <ramos.marialeticiaalves@gmail.com>
2026-01-16 21:17:55 -0300

    adds nuget packages e first configurations

--- BackgroundJobs.Api/Program.cs
+++ BackgroundJobs.Api/Program.cs
@@ -1,3 +1,7 @@
+using Hangfire;
+using Hangfire.Storage.SQLite;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,17 +10,34 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddHangfire(config =>
+{
+    config.UseSimpleAssemblyNameTypeSerializer()
+          .UseRecommendedSerializerSettings()
+          .UseSQLiteStorage(
+              builder.Configuration.GetConnectionString("HangfireDb")
+          );
+});
+builder.Services.AddHangfireServer();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(options =>
+    {
+        options.WithTitle("API Background Jobs")
+               .WithTheme(ScalarTheme.Solarized);
+    });
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseHangfireDashboard("/hangfire");
 
 app.MapControllers();
 
